bb9b46000346ce4164fa3a64a442da35e68d1a0d
SiLeader <sileader.dev@gmail.com>
2018-07-15 14:54:55 +0900

    add flask component

--- pymvc/__init__.py
+++ pymvc/__init__.py
@@ -3,6 +3,8 @@
 
 from .pkg_info import __copyright__, __version__, __license__, __author__
 
+from flask import request, jsonify, abort, redirect, url_for, session
+
 from .controller import Controller, render
 from .router import route, add_route
 from .model import Model, UniqueIdType, IntType, FloatType, StringType, ForeignType, binder
--- pymvc/pkg_info.py
+++ pymvc/pkg_info.py
@@ -3,6 +3,6 @@
 
 
 __copyright__ = "(C) 2018 SiLeader and Cerussite."
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __license__ = "Apache 2.0"
 __author__ = "SiLeader and Cerussite"
