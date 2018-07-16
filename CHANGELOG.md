84d4d9075f023d098dc8263fedaf0a67dfce2ba1
SiLeader <sileader.dev@gmail.com>
2018-07-16 21:51:19 +0900

    merge previous commit

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 Change Log
 =================
 
-## 0.1.1
+## 0.1.2
 ### Added
 + hash function support
 + HashType support
--- pymvc/pkg_info.py
+++ pymvc/pkg_info.py
@@ -3,6 +3,6 @@
 
 
 __copyright__ = "(C) 2018 SiLeader and Cerussite."
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __license__ = "Apache 2.0"
 __author__ = "SiLeader and Cerussite"
